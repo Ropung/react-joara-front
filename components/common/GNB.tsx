f89434ecc6e833974e66ac76d2748271b0a56c35
Gihun <563710@naver.com>
2023-04-14 13:52:39 +0900

    feat: Add react hook form

--- components/common/GNB.tsx
+++ components/common/GNB.tsx
@@ -1,36 +1,62 @@
 import Link from "next/link";
 import Image from "next/image";
 // import { useRouter } from "next/router";
+import { FaSearch } from "react-icons/fa";
+import { GiCancel } from "react-icons/gi";
+import { useRef, useState } from "react";
 
 const GNB = () => {
+  const searchFormRef = useRef<HTMLInputElement>(null);
+  const [isAuth, setAuth] = useState<boolean>(false);
   // const route = useRouter();
+
   return (
     <nav className="flex flex-row justify-between items-center h-20 bg-white px-8 border-b shadow-t-md">
-      <Link href={"/"}>
-        <Image
-          width={280}
-          height={100}
-          className="w-36 cursor-pointer"
-          src={"/logo/ic_nav_home_logo_bl.png"}
-          alt="메인로고"
-        />
-      </Link>
-      <div className="flex flex-row gap-2">
-        <ul className="flex flex-row gap-4 text-2xl cursor-pointer">
+      <div className="flex flex-row gap-6 items-center justify-center">
+        <Link href={"/"}>
+          <Image
+            width={280}
+            height={100}
+            className="w-36 cursor-pointer"
+            src={"/logo/ic_nav_home_logo_bl.png"}
+            alt="메인로고"
+          />
+        </Link>
+        <ul className="flex flex-row gap-4 text-xl cursor-pointer items-center justify-start">
           <li className="focus:text-main hover:text-main">
-            <Link href="/">메뉴1</Link>
+            <Link href="/">웹소설</Link>
           </li>
           <li className="focus:text-main hover:text-main">
-            <Link href="/">메뉴2</Link>
-          </li>
-          <li className="focus:text-main hover:text-main">
-            <Link href="/">메뉴3</Link>
+            <Link href="/">메뉴뭐있을까</Link>
           </li>
         </ul>
-        <div className="flex flex-row gap-2 items-center">
-          <Link href={"/login"}>로그인</Link>
-          <Link href={"/signup"}>회원가입</Link>
-        </div>
+      </div>
+      {/* search */}
+      <div className="w-[300px] flex flex-row justify-between items-center gap-2 boder-black border-2 px-4 py-2 rounded-full">
+        <FaSearch className="text-left text-xl" />
+        <input
+          className="px-2"
+          type="text"
+          placeholder="검색"
+          ref={searchFormRef}
+        />
+        <GiCancel className="text-right text-xl text-gray-200" />
+      </div>
+      <div className="flex flex-row gap-2">
+        {!isAuth ? (
+          <>
+            <Link
+              href={"/login"}
+              className="p-2 bg-default text-main-contra rounded-md hover:bg-main"
+            >
+              로그인
+            </Link>
+          </>
+        ) : (
+          <>
+            <div>로그인된상태</div>
+          </>
+        )}
       </div>
     </nav>
   );
--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,5 +1,130 @@
+import Path from "@/constants/path/routes";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import {
+  AiFillInstagram,
+  AiFillAndroid,
+  AiFillTwitterCircle,
+} from "react-icons/ai";
+import { useForm } from "react-hook-form";
+import Link from "next/link";
+import { useEffect } from "react";
+interface FormValue {
+  email: string;
+  password: string;
+}
+
 const Login = () => {
-  return <div>로긴뜨냐?</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, isDirty, errors },
+  } = useForm<FormValue>();
+  const { SIGNUP } = Path;
+
+  useEffect(() => {
+    console.log(isSubmitting);
+  }, [isSubmitting]);
+
+  return (
+    <div className="w-full min-h-screen flex flex-col gap-2 p-8 justify-start items-center">
+      <main className="w-[400px] max-h-[80vh] flex flex-col gap-4 bg-white p-4 rounded-md shadow-t-sm">
+        <div className="w-full flex flex-1 flex-col gap-16 items-center py-8">
+          <Image
+            width={280}
+            height={100}
+            className="w-36 cursor-pointer"
+            src={"/logo/ic_nav_home_logo_bl.png"}
+            alt="메인로고"
+          />
+
+          <section className="w-full flex flex-col gap-4 items-center justify-center px-8 py-4">
+            {/* 아이디 비밀번호 form 영역 */}
+            <form
+              className="w-full flex flex-col gap-8"
+              onSubmit={handleSubmit((data) => {
+                // FIXME
+                alert(JSON.stringify(data));
+              })}
+            >
+              <div className="w-full flex flex-col gap-2 items-start justify-center">
+                <input
+                  id="email"
+                  className="w-full flex flex-1 border border-gary-200 p-2"
+                  type="email"
+                  placeholder="이메일을 입력해주세요."
+                  aria-invalid={
+                    !isDirty ? undefined : errors.email ? "true" : "false"
+                  }
+                  {...register("email", {
+                    required: "이메일은 필수 입력입니다.",
+                    pattern: {
+                      value: /\S+@\S+\.\S+/,
+                      message: "이메일 형식에 맞지 않습니다.",
+                    },
+                  })}
+                />
+                {errors.email && (
+                  <small role="alert">{errors.email.message}</small>
+                )}
+                <input
+                  id="password"
+                  type="text"
+                  placeholder="비밀번호"
+                  aria-invalid={
+                    !isDirty ? undefined : errors.password ? "true" : "false"
+                  }
+                  className="w-full flex flex-1 border border-gary-200 p-2"
+                  {...register("password", {
+                    required: "비밀번호는 필수 입력입니다.",
+                    pattern: {
+                      value:
+                        /(?=.*\d{1,50})(?=.*[~`!@#$%\^&*()-+=]{1,50})(?=.*[a-zA-Z]{2,50}).{8,16}$/,
+                      message:
+                        "비밀번호를 8~16자로 영문 대소문자, 숫자, 특수기호를 조합해서 사용하세요. ",
+                    },
+                  })}
+                />
+                {errors.password && (
+                  <small role="alert">{errors.password.message}</small>
+                )}
+              </div>
+              {/* 버튼영역 */}
+              <div className="w-full flex flex-col gap-2">
+                <button
+                  type="submit"
+                  disabled={isSubmitting}
+                  className={"w-full p-2 bg-main text-main-contra rounded-md"}
+                  //
+                  onClick={() => {
+                    // FIXME axios 작업
+                  }}
+                >
+                  로그인
+                </button>
+                <Link
+                  href={SIGNUP}
+                  className="text-center p-2 bg-default text-main-contra rounded-md hover:bg-main"
+                >
+                  회원가입
+                </Link>
+              </div>
+            </form>
+
+            <p className="w-full border-b-[0.5px] border-gray-400 bg-black" />
+            {/* Oauth2 */}
+            <div className="w-full flex flex-row gap-4 text-5xl items-center justify-center">
+              <AiFillInstagram className="text-main cursor-pointer border boder-gray-400 rounded-full p-1 hover:text-white hover:bg-main" />
+              <AiFillAndroid className="text-main cursor-pointer border boder-gray-400 rounded-full p-1 hover:text-white hover:bg-main" />
+              <AiFillTwitterCircle className="text-main cursor-pointer border boder-gray-400 rounded-full p-1 hover:text-white hover:bg-main" />
+              <button></button>
+              <button></button>
+            </div>
+          </section>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Login;
