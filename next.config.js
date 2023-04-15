2e8a718c8472b286f7566b37919f5748ee9b677f
Gihun <563710@naver.com>
2023-04-15 21:19:21 +0900

    feat: <img/> apply & view 작업

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "image.yes24.com",
+        port: "",
+        pathname: "/goods/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
