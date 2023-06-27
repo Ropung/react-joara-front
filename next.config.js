4bf6335f5bf0daa200e8118438504879aa0773d2
Gihun <563710@naver.com>
2023-06-27 21:18:46 +0900

    config(next): add host img host

--- next.config.js
+++ next.config.js
@@ -5,12 +5,10 @@ const nextConfig = {
   // basePath: "/react-joara-front",
   reactStrictMode: false,
   images: {
-    domains: ['cdn.myanimelist.net'],
-
+    domains: ["cdn.myanimelist.net"],
     // FIXME 이미지 설정
     // unoptimized: true,
     remotePatterns: [
-      // https://cdn.myanimelist.net/r/100x140/images/anime/1296/96987.webp?s=cd7c77a89344f323491c43a8ae3f172d
       {
         protocol: "http",
         hostname: "cdn.myanimelist.net",
