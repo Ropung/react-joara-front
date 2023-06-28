75761ed4e499c913fc98672e43eeb5b3a38d8adc
Gihun <563710@naver.com>
2023-06-29 01:16:53 +0900

    feat(constants): add episode & comment & reply

--- constants/path/api/index.ts
+++ constants/path/api/index.ts
@@ -15,14 +15,29 @@ const API_PATH = {
   API_BOOK: "/books",
   API_BOOK_ME: "/books/me",
   API_BOOK_FAVOR: "/books/favorite",
+  API_BOOK_RECOMMEND: "/books",
+
   API_BOOK_CREATE: "/books",
   API_BOOK_UPDATE: "/books/",
   API_BOOK_DELETE: "/books/",
 
-  API_BOOK_RECOMMEND: "/books",
-
   // Episodes
   API_EPISODE: "/episode",
+  API_EPISODE_CREATE: "/episode",
+  API_EPISODE_UPDATE: "/episode/",
+  API_EPISODE_DELETE: "/episode/",
+
+  // Comment
+  API_COMMENT: "/comment",
+  API_COMMENT_CREATE: "/comment",
+  API_COMMENT_UPDATE: "/comment/",
+  API_COMMENT_DELETE: "/comment/",
+
+  // Reply
+  API_REPLY: "/reply",
+  API_REPLY_CREATE: "/reply",
+  API_REPLY_UPDATE: "/reply/",
+  API_REPLY_DELETE: "/reply/",
 
   // API_CREATE_PROFILE_IMAGE: "/users/img",
   // API_UPDATE_USER_INFO: "/users/update",
