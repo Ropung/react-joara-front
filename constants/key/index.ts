58dea21492cce4c04b01aace38c5802d62357b6e
inns21 <inns2110@gmail.com>
2023-06-29 09:51:32 +0900

    fix(recommend): add queryKey - BOOK_RECOMMEND_KEY

--- constants/key/index.ts
+++ constants/key/index.ts
@@ -10,5 +10,8 @@ export const BOOK_KEY = "book";
 export const BOOK_CREATE_KEY = "book_create";
 export const BOOK_UPDATE_KEY = "book_update";
 export const BOOK_DELETE_KEY = "book_delete";
+//Recommend
+export const BOOK_RECOMMEND_KEY = "book_recommend";
+
 // Episode
 export const EPISODE_KEY = "episode";
