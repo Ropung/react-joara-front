f4c830eccb148694181deeb24ba7056f44795e76
Gihun <563710@naver.com>
2023-06-30 14:04:32 +0900

    feat(key): add constants

--- constants/key/index.ts
+++ constants/key/index.ts
@@ -1,15 +1,24 @@
 // book
+export const BOOK = "book";
 export const BOOK_FAVORITE_LIST_KEY = "book_favorite";
 export const BOOK_MY_LIST_KEY = "book_me";
 export const BOOK_ONE_KEY = "book_one";
 export const BOOK_RECOMMEND_KEY = "book_recommend";
+
+// comment
+export const COMMENT_KEY = "comment";
 export const BOOK_COMMENT_KEY = "book_comment";
 
 // episode
+export const EPISODE = "episode";
 export const EPISODE_LIST_KEY = "episode_book";
 export const EPISODE_ONE_KEY = "episode_one";
 
 // genre
+export const GENRE = "genre";
 export const GENRE_BOOK_LIST_KEY = "genre_book_list";
 export const GENRE_LIST_KEY = "genre_list";
+
+// MEMBER
+export const MEMBER = "member";
 export const MEMBER_PROFILE_KEY = "member_profile";
